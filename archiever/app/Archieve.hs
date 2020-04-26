78955da45039263b7cc7a4b4409301313e7df291
Shojin <shomasd@gmail.com>
2020-04-27 00:30:58 +0900

    rename Main files

--- archiever/app/Archieve.hs
+++ archiever/app/Archieve.hs
@@ -4,19 +4,26 @@ import qualified Data.List as L
 import qualified Data.Maybe as M
 import qualified Data.Text as T
 import qualified Data.Time.LocalTime as Time
-import qualified System.Directory as Dir
-import qualified System.IO as IO
 import Model
 import Parser
+import qualified System.Directory as Dir
+import qualified System.IO as IO
 
-endsWith :: String -> String -> Bool
-endsWith short long = M.isJust $ L.stripPrefix (reverse short) (reverse long)
+newArticle :: IO ()
+newArticle = do
+  undefined
 
-
-main :: IO ()
-main = do
+archieveArticles :: IO ()
+archieveArticles = do
   files <- Dir.listDirectory "../articles"
   let mds = map ("../articles/" ++) $ filter (endsWith ".md") files
   contents <- mapM IO.readFile mds
   let articles = map (parseContents . T.pack) contents
   return ()
+  where
+    endsWith :: String -> String -> Bool
+    endsWith short long = M.isJust $ L.stripPrefix (reverse short) (reverse long)
+
+main :: IO ()
+main = do
+  undefined
